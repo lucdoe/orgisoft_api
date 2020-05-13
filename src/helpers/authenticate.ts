a4af423a5f023540af55ade64f4aba2ff25b25e6
lucdoe <doerrstein.lucca@yahoo.de>
2020-05-13 12:03:43 +0200

    added types at auth

--- src/helpers/authenticate.ts
+++ src/helpers/authenticate.ts
@@ -1,15 +1,14 @@
 import * as jwt from 'jsonwebtoken'
 import * as dotenv from 'dotenv'
 import { NextFunction, Request, Response } from 'express'
-
 dotenv.config()
 
 const accessTokenSecret: any = process.env.ACCESS_TOKEN_SECRET
 
 export const accessToken = async (req: Request, res: Response, next: NextFunction) => {
 	try {
-		const authHeader = req.headers['authorization']
-		const token = authHeader && authHeader.split(' ')[1]
+		const authHeader: string | undefined = req.headers['authorization']
+		const token: string | undefined = authHeader && authHeader.split(' ')[1]
 		if (token == null) return res.status(401).json(' Unauthorized access! Please log in. ')
 
 		jwt.verify(token, accessTokenSecret, (err: any, user: any) => {
