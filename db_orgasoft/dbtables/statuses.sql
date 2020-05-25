cce2ce950aa9dad825f8fe3fcbb942fcbec588be
Lucca <42179648+lucdoe@users.noreply.github.com>
2020-05-25 23:38:13 +0200

    bye

--- db_orgasoft/dbtables/statuses.sql
+++
@@ -1,7 +0,0 @@
-
-insert into statuses (status_id, status) values (1, 'Aktiv');
-insert into statuses (status_id, status) values (2, 'Passiv');
-insert into statuses (status_id, status) values (3, 'Ehren');
-insert into statuses (status_id, status) values (4, 'Gönner');
-insert into statuses (status_id, status) values (5, 'Trial');
-insert into statuses (status_id, status) values (6, 'Verwaltung');
