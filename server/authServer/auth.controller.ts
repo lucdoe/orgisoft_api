909b0fb9f705449351584f6494d80f472da61e45
Lucca <42179648+lucdoe@users.noreply.github.com>
2020-06-10 01:09:21 +0200

    fixed authentication

--- server/authServer/auth.controller.ts
+++ server/authServer/auth.controller.ts
@@ -1,7 +1,8 @@
-require('dotenv').config()
+import * as dotenv from 'dotenv'
+dotenv.config()
 import * as bcrypt from 'bcrypt'
 import { Request, Response, NextFunction } from 'express'
-import User from './model.user'
+import User from './auth.model.user'
 import jwt from 'jsonwebtoken'
 
 let refreshTokens: string[] = []
@@ -9,29 +10,45 @@ const refreshTokenSecret: any = process.env.REFRESH_TOKEN_SECRET
 const accessTokenSecret: any = process.env.TOKEN_KEY
 
 export const registerUser = async (request: Request, response: Response, next: NextFunction) => {
-	const { firstName, email, password } = request.body
+	const { firstName, lastName, email, password, passwordRepeat } = request.body
 
-	if (await User.findOne({ email })) {
-		response.status(401).json({ error: 'This email already exists!' })
-	} else {
+	const emailAlreadyTaken = (user) => {
+		if (user) response.status(401).json({ error: 'This email already exists!' })
+	}
+	const passwordToShort = () => {
+		response.json({ error: 'Password to short.' })
+	}
+	const passwordIsNotEqual = () => {
+		response.json({ error: 'Passwords do not match.' })
+	}
+
+	if (password.length <= 7) passwordToShort()
+	if (password != passwordRepeat) passwordIsNotEqual()
+
+	try {
+		const checkedUser = await User.findOne({ email })
+		if (checkedUser) emailAlreadyTaken(checkedUser)
 		const hashedPassword = await bcrypt.hash(password, 10)
 
 		await User.create({
 			firstName,
+			lastName,
 			email,
 			password: hashedPassword,
 		})
 
-		const user: object = {
+		const userObject: object = {
 			firstName,
 			email,
 		}
 
-		const accessToken = generateAccessToken(user)
-		const refreshToken: string = jwt.sign(user, refreshTokenSecret)
+		const accessToken = generateAccessToken(userObject)
+		const refreshToken: string = jwt.sign(userObject, refreshTokenSecret)
 
 		refreshTokens.push(refreshToken)
 		response.status(200).json({ accessToken, refreshToken })
+	} catch (error) {
+		console.log(error)
 	}
 }
 
@@ -40,24 +57,32 @@ export const loginUser = async (request: Request, response: Response, next: Next
 	const email: string = request.body.email
 
 	await User.findOne({ email }, async (error, user) => {
-		if (user) {
-			const incomingPassword: string = user['password']
+		if (error) response.status(403).json({ error: 'Unvalid User data try again.', status: 'Unauthorized' })
+
+		if (!user) {
+			response.status(403).json({ error: 'Unvalid User data try again.', status: 'Unauthorized' })
+		} else {
+			const incomingPassword = user['password']
 
 			await bcrypt.compare(password, incomingPassword, (error, result) => {
+				if (error || !result)
+					response.status(403).json({
+						error: 'Unvalid User data try again.',
+						status: 'Unauthorized',
+					})
+
 				if (result) {
 					const userObject = {
 						firstName: user['firstName'],
 						email: user['email'],
 						password: user['password'],
 					}
+
 					const accessToken = generateAccessToken(userObject)
 					const refreshToken: string = jwt.sign(userObject, refreshTokenSecret)
+
 					refreshTokens.push(refreshToken)
 					response.status(200).json({ accessToken, refreshToken })
-				} else if (error) {
-					response.status(403).json({ error: 'Unvalid User data try again.' })
-				} else {
-					response.status(403).json({ error: 'Unvalid User data try again.' })
 				}
 			})
 		}
@@ -67,11 +92,12 @@ export const loginUser = async (request: Request, response: Response, next: Next
 export const refreshLoginToken = (request: Request, response: Response, next: NextFunction) => {
 	const refreshToken: string = request.body.token
 
-	if (refreshToken == null) return response.status(401).send('Unauthorized')
-	if (!refreshTokens) return response.status(403).send('Unauthorized')
+	if (refreshToken == null)
+		return response.status(401).json({ error: 'Log in required', status: 'Unauthorized' })
+	if (!refreshTokens) return response.status(403).json({ error: 'Log in required', status: 'Unauthorized' })
 
 	jwt.verify(refreshToken, refreshTokenSecret, (error, user) => {
-		if (error) return response.status(403).send('Unauthorized')
+		if (error) return response.status(403).json({ error: 'Unvalid credentials!', status: 'Unauthorized' })
 		const accessToken: string = generateAccessToken({ user })
 		response.json({ accessToken })
 	})
@@ -79,7 +105,7 @@ export const refreshLoginToken = (request: Request, response: Response, next: Ne
 
 export const logoutUser = (request: Request, response: Response, next: NextFunction) => {
 	refreshTokens = refreshTokens.filter((token) => token !== request.body.token)
-	response.status(204).send('Unauthorized')
+	response.status(403).json({ message: 'Loged out', status: 'Unauthorized' })
 }
 
 const generateAccessToken = (user: string | object | Buffer): string => {
--- server/authServer/auth.dbConfig.ts
+++ server/authServer/auth.dbConfig.ts
@@ -1,6 +1,11 @@
+import * as dotenv from 'dotenv'
+dotenv.config()
 import mongoose from 'mongoose'
 
-export const createDbConnection = () => {
-	const uri = `mongodb+srv://${process.env.AUTH_DB_PW}:${process.env.AUTH_DB_USER}.mongodb.net/${process.env.AUTH_DB_NAME}?retryWrites=true&w=majority`
-	mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+export const createDbConnection = async () => {
+	const dbPw = process.env.AUTH_DB_PW
+	const dbUser = process.env.AUTH_DB_USER
+	const dbName = process.env.AUTH_DB_NAME
+	const uri = `mongodb+srv://${dbUser}:${dbPw}@cluster0-8reyi.mongodb.net/${dbName}?retryWrites=true&w=majority`
+	await mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
 }
