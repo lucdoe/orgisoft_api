ac004f33eaf491e99826c197bb07624cdc7bdfb3
Lucca <42179648+lucdoe@users.noreply.github.com>
2020-06-15 14:20:50 +0200

    refactored delete member data

--- server/src/members/controllers/controller.deleteMembers.ts
+++ server/src/members/controllers/controller.deleteMembers.ts
@@ -1,5 +1,4 @@
-import { Request, Response, response } from 'express'
-import { getManager } from 'typeorm'
+import { Request, Response } from 'express'
 import { Memberqualifications } from '../models/model.Memberqualification'
 import { Members } from '../models/model.Member'
 import { Positions } from '../models/model.Position'
@@ -7,87 +6,78 @@ import { Statuses } from '../models/model.Status'
 import { Qualifications } from '../models/model.Qualification'
 import { Membergroups } from '../models/model.Membergroup'
 
-const message = (data: { row: number | string; type: string }, response: Response) => {
+const responseWith = (data: { row: number | string; type: string }, response: Response) => {
 	return response.status(200).json({ message: `Succesfully deleted ${data.type} with id of: ${data.row}` })
 }
 
 export const deleteMember = async (request: Request, response: Response) => {
 	const data = { row: request.params.id, type: 'Member' }
 	await Members.delete(data.row)
-	message(data, response)
+	responseWith(data, response)
 }
 
 export const deleteMemberAddress = async (request: Request, response: Response) => {
-	const memberId = request.params.id
-	const manager = getManager()
-	const deleteMemberAddress = {
+	const data = { row: request.params.id, type: 'Member Address' }
+	const update = {
 		addresses: undefined,
 	}
-	await manager.update(Members, memberId, deleteMemberAddress)
-	response.status(200).json(`Succesfully deleted Members Address.`)
+	await Members.update(data.row, update)
+	responseWith(data, response)
 }
 
 export const deleteMemberPosition = async (request: Request, response: Response) => {
-	const memberId = request.params.id
-	const manager = getManager()
-	const deleteMemberPosition = {
+	const data = { row: request.params.id, type: 'Member Position' }
+	const update = {
 		positions: undefined,
 	}
-	await manager.update(Members, memberId, deleteMemberPosition)
-	response.status(200).json(`Succesfully deleted Members Position.`)
+	await Members.update(data.row, update)
+	responseWith(data, response)
 }
 
 export const deleteMemberStatus = async (request: Request, response: Response) => {
-	const memberId = request.params.id
-	const manager = getManager()
-	const deleteMemberStatus = {
+	const data = { row: request.params.id, type: 'Member Status' }
+	const update = {
 		statuses: undefined,
 	}
-	await manager.update(Members, memberId, deleteMemberStatus)
-	response.status(200).json(`Succesfully deleted Members Status.`)
+	await Members.update(data.row, update)
+	responseWith(data, response)
 }
 
 export const deleteMemberMembergroup = async (request: Request, response: Response) => {
-	const memberId = request.params.id
-	const manager = getManager()
-	const deleteMemberMembergroup = {
+	const data = { row: request.params.id, type: 'Member Group' }
+	const update = {
 		membergroups: undefined,
 	}
-	await manager.update(Members, memberId, deleteMemberMembergroup)
-	response.status(200).json(`Succesfully deleted Members Membergroup.`)
+	await Members.update(data.row, update)
+	responseWith(data, response)
 }
 
 export const deleteMemberqualification = async (request: Request, response: Response) => {
-	const manager = getManager()
-	const memberQualificationId = request.params.id
-	await manager.delete(Memberqualifications, memberQualificationId)
-	response.status(200).json(`Succesfully deleted Memberqualification.`)
+	const data = { row: request.params.id, type: 'Member Qualification' }
+	await Memberqualifications.delete(data.row)
+	responseWith(data, response)
 }
 
 export const deletePosition = async (request: Request, response: Response) => {
-	const manager = getManager()
-	const memberPositionId = request.params.id
-	await manager.delete(Positions, memberPositionId)
-	response.status(200).json(`Succesfully deleted Position.`)
+	const data = { row: request.params.id, type: 'Position' }
+	await Positions.delete(data.row)
+	responseWith(data, response)
 }
 
 export const deleteStatus = async (request: Request, response: Response) => {
-	const manager = getManager()
-	const memberStatusId = request.params.id
-	await manager.delete(Statuses, memberStatusId)
-	response.status(200).json(`Succesfully deleted Status.`)
+	const data = { row: request.params.id, type: 'Status' }
+	await Statuses.delete(data.row)
+	responseWith(data, response)
 }
 
 export const deleteQualification = async (request: Request, response: Response) => {
-	const manager = getManager()
-	const qualificationId = request.params.id
-	await manager.delete(Qualifications, qualificationId)
-	response.status(200).json(`Succesfully deleted Qualification.`)
+	const data = { row: request.params.id, type: 'Qualification' }
+	await Qualifications.delete(data.row)
+	responseWith(data, response)
 }
 
 export const deleteMembergroup = async (request: Request, response: Response) => {
-	const manager = getManager()
-	const memberMembergroupId = request.params.id
-	await manager.delete(Membergroups, memberMembergroupId)
-	response.status(200).json(`Succesfully deleted Membergroup.`)
+	const data = { row: request.params.id, type: 'Member Group' }
+	await Membergroups.delete(data.row)
+	responseWith(data, response)
 }
