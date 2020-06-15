9743b2ab21d7970e6e6e4af63e13f3fec816b771
Lucca <42179648+lucdoe@users.noreply.github.com>
2020-06-15 14:06:46 +0200

    Refactored member delete

--- server/src/members/controllers/controller.deleteMembers.ts
+++ server/src/members/controllers/controller.deleteMembers.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from 'express'
+import { Request, Response, response } from 'express'
 import { getManager } from 'typeorm'
 import { Memberqualifications } from '../models/model.Memberqualification'
 import { Members } from '../models/model.Member'
@@ -7,11 +7,14 @@ import { Statuses } from '../models/model.Status'
 import { Qualifications } from '../models/model.Qualification'
 import { Membergroups } from '../models/model.Membergroup'
 
+const message = (data: { row: number | string; type: string }, response: Response) => {
+	return response.status(200).json({ message: `Succesfully deleted ${data.type} with id of: ${data.row}` })
+}
+
 export const deleteMember = async (request: Request, response: Response) => {
-	const memberId = request.params.id
-	const manager = getManager()
-	await manager.delete(Members, memberId)
-	response.status(200).json(`Succesfully deleted Member.`)
+	const data = { row: request.params.id, type: 'Member' }
+	await Members.delete(data.row)
+	message(data, response)
 }
 
 export const deleteMemberAddress = async (request: Request, response: Response) => {
