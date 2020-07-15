294a63f7e113f2ca24fa26e295622c9b37354677
Lucca <42179648+lucdoe@users.noreply.github.com>
2020-07-15 18:27:35 +0200

    filtering for inventoryitems

--- server/src/inventoryitems/controllers/controller.readInventorys.ts
+++ server/src/inventoryitems/controllers/controller.readInventorys.ts
@@ -2,8 +2,17 @@ import { Request, Response } from 'express'
 import { Inventoryitems } from '../models/model.Inventoryitem'
 
 export const allInventoryitems = async (request: Request, response: Response) => {
+	const skip = +request.query.skip
+	const take = +request.query.take
+	const { desc } = request.query
+	const orderDirection = desc == '1' ? 'DESC' : 'ASC'
 	const result = await Inventoryitems.find({
 		relations: ['members', 'inventorygroups', 'inventoryplaces'],
+		order: {
+			inventoryitem: orderDirection,
+		},
+		skip: skip,
+		take: take,
 	})
 	return response.status(200).json(result)
 }
